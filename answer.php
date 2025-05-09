b4280986699f911f000e2fbc3ba6415c2657bed8
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-09 17:22:01 +0000

    init

--- answer.php
+++ answer.php
@@ -31,33 +31,34 @@
       <div class="right-image">
         <img src="./images/table.jpg" alt="division table image" width="500" />
       </div>
-      <br />
-      <div class="page-content-guide">Please enter two numbers. </div>
-      <div class="page-content">
-        <form action="./answer.php" method="GET">
-          <label>Number: </label>
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="number-one" />
-            <label class="mdl-textfield__label" for="number-one">First number here...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <label> ÷ </label>
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="number-two" />
-            <label class="mdl-textfield__label" for="number-two">First number here...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
+      <div class="page-content-answer">
+        <div id="answer">
+          <?php
+          // input
+          $counter = 0;
+          $numberOne = $_GET['number-one'];
+          $numberTwo = $_GET['number-two'];
+          $originalNumberOne = $numberOne;
+
+          //process
+          while ($numberOne >= $numberTwo ) {
+            $counter ++;
+            $numberOne -= $numberTwo;
+
+            if ($numberOne < $numberTwo) {
+              break;
+            }
+          }
+
+          //output
+          echo $originalNumberOne . ' ÷ ' . $numberTwo . ' is: ' . $counter . ' R ' . $numberOne . '.'
+          ?>
+        </div>
       </div>
-      <br />
-      <!-- Accent-colored raised button with ripple -->
-      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-        type="submit">
-        Check
-      </button>
-      </form>
-  </div>
-  </main>
+      <div class="page-content-return">
+        <a href="./index.php">Return ...</a>
+      </div>
+    </main>
   </div>
 </body>
 
